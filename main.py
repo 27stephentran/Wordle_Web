dddbdb814b10b251d7ce39c5761b12b601fe5b1c
27stephentran <121224237+27stephentran@users.noreply.github.com>
2024-09-27 13:53:30 +0000

    Week 02

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    image_url = "/workspaces/Hangout_Planner/templates/index.html" 
-    return render_template("index.html", image_url = image_url)
+    image_url = "https://github.com/27stephentran/Hangout_Planner/blob/main/templates/capybara.png" 
+    return render_template("index_2.html", image_url = image_url)
 
 @app.route("/input_page")
 def input_page():
